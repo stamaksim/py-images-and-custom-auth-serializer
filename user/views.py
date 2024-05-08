0ca42846c4050b56e0b8412562def71e536861cd
Maksim Stahovskij <maksss196@gmail.com>
2024-05-08 20:31:26 +0300

    py-images-and-custom-auth-serializer

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -9,10 +10,12 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = ()
 
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
